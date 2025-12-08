d9ac6b1c37ae8a7032dade655fac7881c57590d8
FullStackDj <full.stackdj@gmail.com>
2025-12-09 00:57:36 +0200

    Added SPA routing and placeholder pages for core flows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
+import Header from "./components/Header.jsx";
+import {Route, Routes} from "react-router-dom"
+import Home from "./pages/Home.jsx";
+import Collection from "./pages/Collection.jsx";
+import Contact from "./pages/Contact.jsx";
+import About from "./pages/About.jsx";
+import Cart from "./pages/Cart.jsx";
+import PlaceOrder from "./pages/PlaceOrder.jsx";
+import Orders from "./pages/Orders.jsx";
+import Verify from "./pages/Verify.jsx";
+import Login from "./pages/Login.jsx";
+import Product from "./pages/Product.jsx";
+
 export default function App() {
   return (
     <div>
-      <Header />
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/collection" element={<Collection/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/product/:productId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/place-order" element={<PlaceOrder/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/verify" element={<Verify/>}/>
+      </Routes>
     </div>
   )
 }

3f31053ee95fb10c2f35c9eddb1297b5fa0d48a3
FullStackDj <full.stackdj@gmail.com>
2025-12-11 23:10:35 +0200

    Crafted feature highlights with icons for core benefits

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+import NewArrivals from "../components/NewArrivals.jsx";
+import PopularProducts from "../components/PopularProducts.jsx";
+import Features from "../components/Features.jsx";
 
 const Home = () => {
   return (
-    <Hero />
+    <>
+      <Hero/>
+      <NewArrivals/>
+      <PopularProducts/>
+      <Features/>
+      <Footer/>
+    </>
   );
 };
 

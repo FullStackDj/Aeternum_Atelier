c72146b7b959c920e10abc350fdd8deae9887f32
FullStackDj <full.stackdj@gmail.com>
2025-12-14 09:10:00 +0200

    Added delivery form state and basic input bindings

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,9 +1,27 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {ShopContext} from '../context/ShopContext'
 import Footer from '../components/Footer'
 
 const PlaceOrder = () => {
   const {navigate} = useContext(ShopContext)
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    street: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    country: '',
+    phone: '',
+  })
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setFormData((data) => ({...data, [name]: value}))
+  }
 
   return (
     <section>
@@ -12,6 +30,35 @@ const PlaceOrder = () => {
           <div className='flex flex-col xl:flex-row gap-20 xl:gap-28'>
             <div className='flex flex-1 flex-col gap-3 text-[95%]'>
               <h3 className='h3'>Delivery Information</h3>
+              <div className='flex gap-3'>
+                <input
+                  onChange={onChangeHandler}
+                  value={formData.firstName}
+                  type='text'
+                  name='firstName'
+                  placeholder='First Name'
+                  required
+                  className='ring-1 ring-slate-900/15 p-1 pl-3 rounded-sm bg-primary outline-none'
+                />
+                <input
+                  onChange={onChangeHandler}
+                  value={formData.lastName}
+                  type='text'
+                  name='lastName'
+                  placeholder='Last Name'
+                  required
+                  className='ring-1 ring-slate-900/15 p-1 pl-3 rounded-sm bg-primary outline-none'
+                />
+              </div>
+              <input
+                onChange={onChangeHandler}
+                value={formData.email}
+                type='email'
+                name='email'
+                placeholder='Email'
+                required
+                className='ring-1 ring-slate-900/15 p-1 pl-3 rounded-sm bg-primary outline-none'
+              />
             </div>
             <div className='flex flex-1 flex-col'>
               <button type='button' onClick={() => navigate('/cart')} className='btn-secondary'>

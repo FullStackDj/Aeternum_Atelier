cc0cc16a17804fd1a12e73477390c3141f55ea47
FullStackDj <full.stackdj@gmail.com>
2025-12-15 08:08:52 +0200

    Fetched orders list from API by token

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react'
+import {ShopContext} from '../context/ShopContext'
+import axios from 'axios'
 
 const Orders = () => {
-  return (
-    <div>
-      Orders
-    </div>
-  );
-};
+  const {backendUrl, token, currency} = useContext(ShopContext)
+  const [orderData, setOrderData] = useState([])
 
-export default Orders;
+  const loadOrderData = async () => {
+    try {
+      if (!token) return null
+      const response = await axios.post(
+        backendUrl + '/api/order/userorders',
+        {},
+        {headers: {token}}
+      )
+      if (response.data.success) {
+        let allOrdersItem = []
+        response.data.orders.map((order) => {
+          order.items.map((item) => {
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentMethod'] = order.paymentMethod
+            item['date'] = order.date
+            allOrdersItem.push(item)
+          })
+        })
+        setOrderData(allOrdersItem.reverse())
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    loadOrderData()
+  }, [token])
+
+  return (
+    <section>
+      <div className='max-padd-container'>
+        <div className='max-padd-container py-10 bg-white rounded-2xl my-6 max-xl:mt-8'>
+          <div className=''>
+            <h3 className='h3'>Orders <span className='text-secondary'>List</span></h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Orders

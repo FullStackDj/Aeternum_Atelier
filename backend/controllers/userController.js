8d58e3e1b279cd7e61d0248e7fff3639e5aadd2b
FullStackDj <full.stackdj@gmail.com>
2025-12-18 08:11:10 +0200

    Added registerUser validation, password hashing and JWT token issuance

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import userModel from "../models/userModel.js";
+import validator from "validator";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
 
@@ -29,4 +30,44 @@ const loginUser = async (req, res) => {
   }
 }
 
-export {loginUser}
+const registerUser = async (req, res) => {
+  try {
+    const {name, email, password} = req.body
+    const exists = await userModel.findOne({email})
+
+    if (exists) {
+      return res.json({success: false, message: "User already exists"})
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({success: false, message: "Enter a valid email"})
+    }
+
+    if (password.length < 8) {
+      return res.json({success: false, message: "Password must be at least 8 characters"})
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    const user = await newUser.save()
+    const token = createToken(user._id)
+
+    res.json({success: true, token})
+  } catch (error) {
+    console.log(error)
+    res.json({success: false, error: error.message})
+  }
+}
+
+const adminLogin = async (req, res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin}

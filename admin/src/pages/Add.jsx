d90728d344c3256aa23c762a03a5f1c5f87660e2
FullStackDj <full.stackdj@gmail.com>
2025-12-22 07:00:31 +0200

    Scaffolded Add Product form component with initial state

---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const Add = ({token}) => {
+
+  return (
+    <form className='pl-8'>
+      <div className='flex flex-col gap-y-2 medium-15'>
+      </div>
+    </form>
+  )
+}
+
+export default Add

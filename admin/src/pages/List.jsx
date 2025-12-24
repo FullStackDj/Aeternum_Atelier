16e0e4b98defbe9b24d65c11c450e2817ea40ed4
FullStackDj <full.stackdj@gmail.com>
2025-12-24 07:27:58 +0200

    Implemented product list fetch via API

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,29 @@
-import React, {useState} from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import {backend_url} from '../App'
+import {toast} from 'react-toastify'
 
 const List = () => {
+
   const [list, setList] = useState([])
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backend_url + '/api/product/list')
+      if (response.data.success) {
+        setList(response.data.products)
+      } else {
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchList()
+  }, [])
 
   return (
     <div className='pl-8'>

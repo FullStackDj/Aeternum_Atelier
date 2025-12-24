c546a2739480022ed920188acdd2470040b6f5b1
FullStackDj <full.stackdj@gmail.com>
2025-12-24 08:03:17 +0200

    Added product removal request and refresh

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import React, {useEffect, useState} from 'react'
-import {backend_url} from '../App'
+import {backend_url, currency} from '../App'
 import {toast} from 'react-toastify'
+import {TbTrash} from 'react-icons/tb'
 
-const List = () => {
+const List = ({token}) => {
 
   const [list, setList] = useState([])
 
@@ -12,6 +13,21 @@ const List = () => {
       const response = await axios.get(backend_url + '/api/product/list')
       if (response.data.success) {
         setList(response.data.products)
+      } else {
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(backend_url + '/api/product/remove', {id}, {headers: {token}})
+      if (response.data.success) {
+        toast.success(response.data.message)
+        await fetchList()
       } else {
         toast.error(response.data.message)
       }
@@ -30,8 +46,16 @@ const List = () => {
       <h3 className='h3'>All Products List</h3>
       <div className='flex flex-col gap-2 pt-4'>
         {list.map((item) => (
-          <div key={item._id} className='p-2 bg-white rounded-xl'>
-            <h5 className='text-sm font-semibold'>{item.name}</h5>
+          <div key={item._id} className='flex items-center justify-between gap-3 p-2 bg-white rounded-xl'>
+            <div className='flex items-center gap-3'>
+              <img src={item.image[0]} alt='' className='w-12 rounded-lg'/>
+              <h5 className='text-sm font-semibold'>{item.name}</h5>
+            </div>
+            <div className='flex items-center gap-6'>
+              <p className='text-sm font-semibold'>{item.category}</p>
+              <p className='text-sm font-semibold'>{currency}{item.price}</p>
+              <TbTrash onClick={() => removeProduct(item._id)} className='cursor-pointer text-lg'/>
+            </div>
           </div>
         ))}
       </div>

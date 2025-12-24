99241708f339c25640c2349d61b75267ff33c690
FullStackDj <full.stackdj@gmail.com>
2025-12-24 08:35:25 +0200

    Finalized responsive admin product list layout

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -16,6 +16,7 @@ const List = ({token}) => {
       } else {
         toast.error(response.data.message)
       }
+
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -25,12 +26,14 @@ const List = ({token}) => {
   const removeProduct = async (id) => {
     try {
       const response = await axios.post(backend_url + '/api/product/remove', {id}, {headers: {token}})
+
       if (response.data.success) {
         toast.success(response.data.message)
         await fetchList()
       } else {
         toast.error(response.data.message)
       }
+
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -45,17 +48,23 @@ const List = ({token}) => {
     <div className='pl-8'>
       <h3 className='h3'>All Products List</h3>
       <div className='flex flex-col gap-2 pt-4'>
+        <div
+          className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border text-secondary bg-white bold-14 sm:bold-16 rounded'>
+          <h5>Image</h5>
+          <h5>Name</h5>
+          <h5>Category</h5>
+          <h5>price</h5>
+          <h5>Remove</h5>
+        </div>
         {list.map((item) => (
-          <div key={item._id} className='flex items-center justify-between gap-3 p-2 bg-white rounded-xl'>
-            <div className='flex items-center gap-3'>
-              <img src={item.image[0]} alt='' className='w-12 rounded-lg'/>
-              <h5 className='text-sm font-semibold'>{item.name}</h5>
-            </div>
-            <div className='flex items-center gap-6'>
-              <p className='text-sm font-semibold'>{item.category}</p>
-              <p className='text-sm font-semibold'>{currency}{item.price}</p>
-              <TbTrash onClick={() => removeProduct(item._id)} className='cursor-pointer text-lg'/>
-            </div>
+          <div key={item._id}
+               className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 p-1 bg-white rounded-xl'>
+            <img src={item.image[0]} alt='' className='w-12 rounded-lg'/>
+            <h5 className='text-sm font-semibold'>{item.name}</h5>
+            <p className='text-sm font-semibold'>{item.category}</p>
+            <div className='text-sm font-semibold'>{currency}{item.price}</div>
+            <div><TbTrash onClick={() => removeProduct(item._id)}
+                          className='text-right md:text-center cursor-pointer text-lg'/></div>
           </div>
         ))}
       </div>
